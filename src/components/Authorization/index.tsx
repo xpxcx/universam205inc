d940aaa872aaf6060a9c48de05f5a33fd104b51c
xpxcx <egorov11012005@gmail.com>
2025-02-11 18:22:11 +0500

    Project complete1.1

--- src/components/Authorization/index.tsx
+++ src/components/Authorization/index.tsx
@@ -40,61 +40,41 @@ export const Authorization = () => {
         }   
     }
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            
-        <div className={styles.auhorization}>
-            <div className={styles.auhorizationBlock}>
-            <p className={styles.title}>Авторизация</p>
-
-            <div className={styles.dataBlock}>
-                    <input 
-                    {...register('login', {
-                        required: 'Логин обязателен'
-                    })}
-                    className={styles.inputLogin} 
-                    type="text" 
-                    placeholder='Логин' 
-                    onFocus={() => setErrorMessage('')}
-                    />
-                    <input 
-                    {...register('password', { 
-                        required: 'Пароль обязателен'
-                    }
-                    )}
-                    className={styles.inputPassword} 
-                    type="password" 
-                    placeholder='Пароль' 
-                    onFocus={() => setErrorMessage('')}/>
-                </div>
+        <div className={styles.authorization}>
+            <div className={styles.authorizationBlock}>
+                <p className={styles.title}>Авторизация</p>
                 
-                {errorMessage === 'Пользователь не найден' &&
-                    (<div className={styles.registration}>
-                        <p className={styles.questions}>Похоже, Вы ещё не зарегистрированы.</p>
-                        <Link to='/registration'>
-                                <p className={styles.linkToRegistr} >Зарегистрироваться</p> 
-                        </Link> 
-                    </div>)
-                }
-                {errorMessage === 'Неверый пароль' &&
-                    <div className={styles.registration}>
-                        <p className={styles.questions}>{errorMessage}</p>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className={styles.dataBlock}>
+                        <input 
+                            className={styles.inputLogin}
+                            type="text"
+                            placeholder="Логин"
+                            {...register('login', {
+                                required: true
+                            })}
+                            onFocus={() => setErrorMessage('')}
+                        />
+                        <input 
+                            className={styles.inputPassword}
+                            type="password"
+                            placeholder="Пароль"
+                            {...register('password', {
+                                required: true
+                            })}
+                            onFocus={() => setErrorMessage('')}
+                        />
+                        {errorMessage && <p className={styles.error}>{errorMessage}</p>}
+                        <button className={styles.signInBtn} type="submit">
+                            Войти
+                        </button>
+                        <div className={styles.signUpBlock}>
+                            <p>Нет аккаунта?</p>
+                            <Link to="/registration">Зарегистрироваться</Link>
+                        </div>
                     </div>
-                }
-                {errorMessage === '' && 
-                    <div className={styles.registration}>
-
-                    <p className={styles.questions}>Нет аккаунта?</p>
-                    <Link to='/registration'>
-                        <p className={styles.linkToRegistr} >Зарегистрироваться</p> 
-                    </Link>
-                    </div>
-                }
-                
-                <button type='submit' className={styles.signInBtn}>Войти</button>
-                
+                </form>
             </div>
         </div>
-        </form>
-
     );
 };
