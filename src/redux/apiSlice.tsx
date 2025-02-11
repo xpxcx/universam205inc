670ee42b02d37fe1f889d7cf7626973dc5b32ca8
xpxcx <egorov11012005@gmail.com>
2025-02-11 17:35:56 +0500

    Project complete1

--- src/redux/apiSlice.tsx
+++ src/redux/apiSlice.tsx
@@ -87,11 +87,12 @@ interface ProductResponse {
 
 export const apiSlice = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/api',
+    baseQuery: fetchBaseQuery({ 
+        baseUrl: 'http://localhost:3001/api',
         prepareHeaders: (headers) => {
             const token = localStorage.getItem('token');
             if(token) {
-                headers.set('Authorization', `Bearer ${token}`)
+                headers.set('Authorization', `Bearer ${token}`);
             }
             return headers;
         }
